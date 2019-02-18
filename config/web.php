633f5e868d198747cf08dfd4ed5b545ca4900e20
roman2211 <89628270095@yandex.ru>
2019-02-18 22:22:39 +0700

    lesson4

--- config/web.php
+++ config/web.php
@@ -6,12 +6,15 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log', 'eventController'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'eventController' => [
+            'class' => 'app\controllers\EventController',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'nwCNzQ6jUs0B5DdijQWbq0iS7lZyeCzH',
